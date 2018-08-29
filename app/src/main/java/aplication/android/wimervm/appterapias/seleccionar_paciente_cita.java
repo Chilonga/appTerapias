fdc047b2507b8663930b6fe50c80171274d3c58a
Chilonga <paulinowimer@gmail.com>
2018-08-29 13:33:10 -0430

    Modificado el design de reservar citas

--- app/src/main/java/aplication/android/wimervm/appterapias/seleccionar_paciente_cita.java
+++ app/src/main/java/aplication/android/wimervm/appterapias/seleccionar_paciente_cita.java
@@ -1,7 +1,12 @@
 package aplication.android.wimervm.appterapias;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.speech.RecognizerIntent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -12,11 +17,17 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.ArrayAdapter;
+import android.widget.CalendarView;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.SearchView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -31,6 +42,8 @@ import com.google.firebase.database.ValueEventListener;
 import com.miguelcatalan.materialsearchview.MaterialSearchView;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
 public class seleccionar_paciente_cita extends AppCompatActivity {
 
@@ -41,6 +54,14 @@ public class seleccionar_paciente_cita extends AppCompatActivity {
     MaterialSearchView searchView;
     private TextView txtResultado;
     String online_user_id;
+    private DatabaseReference consultaDatabase,HorarioDatabase;
+    private TextView tVNombre,edtFecha;
+    private Spinner consultaSpiner,HorarioSpiner;
+    final Calendar calendario = Calendar.getInstance();
+    String fecha_calendario,fecha_seleccionada;
+    int año=calendario.get(Calendar.YEAR);
+    int mes=calendario.get(Calendar.MONTH)+1;
+    int dias=calendario.get(Calendar.DAY_OF_MONTH);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -177,17 +198,9 @@ public class seleccionar_paciente_cita extends AppCompatActivity {
                         @Override
                         public void onClick(View view) {
                             try {
-                                String id = users.getId().toString();
-                                if (searchView.isSearchOpen()) {
-                                    searchView.closeSearch();
-                                    Intent intent = new Intent(seleccionar_paciente_cita.this, Reservar_Cita.class);
-                                    intent.putExtra("user_id", id);
-                                    startActivity(intent);
-                                } else {
-                                    Intent intent = new Intent(seleccionar_paciente_cita.this, Reservar_Cita.class);
-                                    intent.putExtra("user_id", id);
-                                    startActivity(intent);
-                                }
+                                InputMethodManager imm = (InputMethodManager) getApplication().getSystemService(Context.INPUT_METHOD_SERVICE);
+                                imm.hideSoftInputFromWindow(searchView.getWindowToken(), 0);
+                                Reservar_Cita(users.getNombre()+ " "+users.getApellido());
                             }catch (Exception e){
                                 Toast.makeText(seleccionar_paciente_cita.this, e.toString(), Toast.LENGTH_SHORT).show();
                             }
@@ -202,25 +215,150 @@ public class seleccionar_paciente_cita extends AppCompatActivity {
         }
     }
 
-    public static class UsersViewHolder extends RecyclerView.ViewHolder {
+    public void Reservar_Cita(String nombreyApellido){
+        try{
+            LayoutInflater inflater = getLayoutInflater();
+            final View dialoglayout = inflater.inflate(R.layout.activity_reservar_citas, null);
 
-        View mView;
+            consultaDatabase = FirebaseDatabase.getInstance().getReference().child("Tipo_Consulta");
+            consultaDatabase.keepSynced(true);
 
-        public UsersViewHolder(View itemView) {
-            super(itemView);
+            HorarioDatabase = FirebaseDatabase.getInstance().getReference().child("Horario");
+            HorarioDatabase.keepSynced(true);
 
-            mView = itemView;
+            tVNombre=(TextView)dialoglayout.findViewById(R.id.textNombreCita);
+            consultaSpiner=(Spinner)dialoglayout.findViewById(R.id.spinnerConsulta);
+            HorarioSpiner=(Spinner)dialoglayout.findViewById(R.id.spinnerHorarioCita);
+            edtFecha=(TextView) dialoglayout.findViewById(R.id.editTFechaCita);
+
+            tVNombre.setText(nombreyApellido);
+
+            fecha_calendario= String.valueOf(dias+"/"+mes+"/"+año);
+
+            final AlertDialog.Builder builder = new AlertDialog.Builder(seleccionar_paciente_cita.this);
+            builder.setView(dialoglayout);
+            builder.setCancelable(false);
+            builder.setNeutralButton("Cerrar", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.cancel();
+                }
+            });
+            builder.show();
+
+            edtFecha.setText(fecha_calendario);
+            spinerConsulta();
+            spinerHorario();
+
+        }catch (Exception e){
+            Toast.makeText(seleccionar_paciente_cita.this,"Error al abrir la fecha",Toast.LENGTH_LONG).show();
         }
+    }
 
+    public void spinerHorario(){
+        try {
+            HorarioDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
 
-        public void setCedula(String cedula){
-            TextView userStatusView = (TextView) mView.findViewById(R.id.txtCedulaPacientes);
-            userStatusView.setText(cedula);
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    try {
+                        final List<String> areas = new ArrayList<String>();
+
+                        for (DataSnapshot areaSnapshot : dataSnapshot.getChildren()) {
+                            String areaName = areaSnapshot.child("hora").getValue(String.class);
+                            areas.add(areaName);
+                        }
+
+                        ArrayAdapter<String> areasAdapter = new ArrayAdapter<String>(seleccionar_paciente_cita.this, android.R.layout.simple_spinner_item, areas);
+                        areasAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                        HorarioSpiner.setAdapter(areasAdapter);
+
+                    } catch (Exception e) {
+                        Toast.makeText(seleccionar_paciente_cita.this, "Error al cargar la data", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                    Toast.makeText(seleccionar_paciente_cita.this, "Error de la base de datos", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }catch (Exception e){
+            Toast.makeText(seleccionar_paciente_cita.this, "Error de la base de datos", Toast.LENGTH_SHORT).show();
         }
+    }
 
-        public void setNombre(String nombre){
-            TextView userStatusView = (TextView) mView.findViewById(R.id.txtPaciente);
-            userStatusView.setText(nombre);
+    public void spinerConsulta(){
+        try {
+            consultaDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    try {
+                        final List<String> areas = new ArrayList<String>();
+
+                        for (DataSnapshot areaSnapshot : dataSnapshot.getChildren()) {
+                            String areaName = areaSnapshot.child("tipo").getValue(String.class);
+                            areas.add(areaName);
+                        }
+
+                        ArrayAdapter<String> areasAdapter = new ArrayAdapter<String>(seleccionar_paciente_cita.this, android.R.layout.simple_spinner_item, areas);
+                        areasAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                        consultaSpiner.setAdapter(areasAdapter);
+
+                    } catch (Exception e) {
+                        Toast.makeText(seleccionar_paciente_cita.this, "Error al cargar la data", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                    Toast.makeText(seleccionar_paciente_cita.this, "Error de la base de datos", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }catch (Exception e){
+            Toast.makeText(seleccionar_paciente_cita.this, "Error de la base de datos", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void fechaCalendario(){
+        try{
+            LayoutInflater inflater = getLayoutInflater();
+            final View dialoglayout = inflater.inflate(R.layout.fecha_calendar, null);
+
+            final CalendarView calendarView;
+
+            calendarView=(CalendarView) dialoglayout.findViewById(R.id.calendarView_fecha);
+
+            calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+                @Override
+                public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                    int monthh=month+1;
+                    fecha_seleccionada=dayOfMonth+"/"+monthh+"/"+year;
+                }
+            });
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(seleccionar_paciente_cita.this);
+            builder.setView(dialoglayout);
+
+            builder.setView(dialoglayout)
+                    .setPositiveButton("Aceptar",new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            edtFecha.setText(fecha_seleccionada);
+                            dialog.cancel();
+                        }
+                    })
+                    .setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.cancel();
+                        }
+                    });
+            builder.show();
+
+        }catch (Exception e){
+            Toast.makeText(seleccionar_paciente_cita.this,"Error al abrir la fecha",Toast.LENGTH_LONG).show();
         }
     }
 
@@ -242,6 +380,27 @@ public class seleccionar_paciente_cita extends AppCompatActivity {
             }
             default:
                 return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public static class UsersViewHolder extends RecyclerView.ViewHolder {
+
+        View mView;
+
+        public UsersViewHolder(View itemView) {
+            super(itemView);
+
+            mView = itemView;
+        }
+
+        public void setCedula(String cedula){
+            TextView userStatusView = (TextView) mView.findViewById(R.id.txtCedulaPacientes);
+            userStatusView.setText(cedula);
+        }
+
+        public void setNombre(String nombre){
+            TextView userStatusView = (TextView) mView.findViewById(R.id.txtPaciente);
+            userStatusView.setText(nombre);
         }
     }
 }
